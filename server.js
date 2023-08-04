23cef7189373ef69f03939d9f736adc8b3d43a14
James Jilhaney <notail2111@gmail.com>
2023-08-04 11:18:14 +0800

    Fetch territories from Netzwelt API

--- server.js
+++ server.js
@@ -7,3 +7,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/places', async (req, res) => {
+  try {
+
+    const response = await fetch("https://netzwelt-devtest.azurewebsites.net/Territories/All");
+
+
+    if (!response.ok) {
+      console.log(response.status)
+      return res.status(response.status).json({ message: 'Error: Invalid credentials' });
+    }
+
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (error) {
+
+    console.error('Error fetching data:', error.message);
+    res.status(500).json({ message: 'Internal server error' });
+
+  }
+});
+
